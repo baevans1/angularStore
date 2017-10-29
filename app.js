e6e3e2259bf383d0b766857a5402250b8abc30e9
Beth Evans <baevans_87@hotmail.com>
2017-10-28 23:11:02 -0500

    testing directives,  not working locally

--- app.js
+++ app.js
@@ -1,11 +1,51 @@
 (function() {
   var app = angular.module('store', []);
 
-
-
   app.controller('StoreController', function(){
     this.product = gems;
   });
+  app.controller("PanelController", function() {
+    this.tab = 1;
+
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+
+    };
+    this.isSelected = function(checkTab) {
+      return this.tab === checkTab;
+    };
+  });
+
+  app.controller('GalleryController', function(){
+      this.current = 0;
+
+      this.setCurrent = function(newGallery){
+        this.current = newGallery || 0;
+      };
+    });
+
+  app.controller('ReviewController', function(){
+        this.review = {};
+
+        this.addReview = function(product) {
+          this.review.createdOn = Date.now();
+          product.reviews.push(this.review);
+          this.review = {};
+        };
+
+    });
+    app.directive("productSpecs", function() {
+      return {
+        restrict: 'E',
+        templateUrl: "product-specs.html"
+      };
+    });
+    app.directive("productTitle", function() {
+      return {
+        restrict: 'E',
+        templateUrl: "product-title.html"
+      };
+    });
 
   var gems = [
   {
@@ -23,12 +63,14 @@
     {
       stars: 5,
       body: "such a beautiful piece",
-      author: "carmensandy@yahoo.com"
+      author: "carmensandy@yahoo.com",
+      createdOn: 6-5-16
     },
     {
       stars: 5,
       body: "It glimmers so bright in the sun.",
-      author: "rougeetrois@plie.fr"
+      author: "rougeetrois@plie.fr",
+      createdOn: 8-12-15
     }
   ]
   },
@@ -48,45 +90,18 @@
       {
         stars: 4,
         body: "I love the realistic feel of a diamond.",
-        author: "luna@lovegood.org"
+        author: "luna@lovegood.org",
+        createdOn: 1-7-12
       },
       {
         stars: 2,
         body: "It's too heavy to wear.",
-        author: "miriam@complainer.com"
+        author: "miriam@complainer.com",
+        createdOn: 5-13-14
       },
     ]
  },
   ]
 
-app.controller("PanelController", function() {
-  this.tab = 1;
 
-  this.selectTab = function(setTab) {
-    this.tab = setTab;
-
-  };
-  this.isSelected = function(checkTab) {
-    return this.tab === checkTab;
-  };
-});
-
-app.controller('GalleryController', function(){
-    this.current = 0;
-
-    this.setCurrent = function(newGallery){
-      this.current = newGallery || 0;
-    };
-  });
-
-app.controller('ReviewController', function(){
-      this.review = {};
-
-      this.addReview = function(product) {
-        product.reviews.push(this.review);
-        this.review = {};
-      };
-
-  });
-
-})();
+})(); //end
